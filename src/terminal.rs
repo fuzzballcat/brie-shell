0a993a306691006b3cf400b3ad5b0c45199ae917
fuzzballcat <57006511+fuzzballcat@users.noreply.github.com>
2023-04-11 15:26:48 -0700

    Update terminal.rs

--- src/terminal.rs
+++ src/terminal.rs
@@ -115,7 +115,7 @@ fn main() {
     if let Ok(path) = env::current_dir() { 
       let sfmt = format!("{1}{0} ", prompt, path.display());
       pathlen = sfmt.len();
-      write!(stdout, "{1}{}{2}", sfmt, termion::color::Fg(termion::color::Rgb(255, 120, 0)), Colors::Reset).unwrap();
+      write!(stdout, "{1}{}{2}", sfmt, termion::color::Fg(termion::color::BrightRed), Colors::Reset).unwrap();
       stdout.flush().unwrap();
     } else {
       write!(stdout, "\x1b[31mERR:\x1b[0m Cannot fetch current directory!\n\r").unwrap();
@@ -220,7 +220,7 @@ fn main() {
       stdout.flush().unwrap();
       
       if !is_final {
-        write!(stdout, "{}", termion::color::Fg(termion::color::Rgb(80, 80, 80))).unwrap();
+        write!(stdout, "{}{}", Colors::Reset, termion::color::Fg(termion::color::Rgb(80, 80, 80))).unwrap();
       }
       
       // exit raw mode for the moment — to make normal term. cmds. work
@@ -324,7 +324,7 @@ fn main() {
         Err(e) => { println!("\x1b[31m{}\x1b[0m", e); std::process::exit(1); }
       };
 
-      write!(stdout, "{}{}", termion::color::Fg(termion::color::Reset), termion::color::Fg(termion::color::White)).unwrap();
+      write!(stdout, "{}{}", termion::color::Fg(termion::color::Reset), termion::color::Fg(termion::color::BrightWhite)).unwrap();
       if !is_final {
         write!(stdout, "\r").unwrap();
         if linesdown > 0 { write!(stdout, "{}", termion::cursor::Up(linesdown as u16)).unwrap(); }
